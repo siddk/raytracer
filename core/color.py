7e174cf8f75f920ac390176b0c278ddc18eff87e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-20 01:23:38 -0800

    Adding color object

---
+++ core/color.py
@@ -0,0 +1,13 @@
+"""
+Color object
+"""
+
+class Color (object):
+	"""
+	Color object initialized with rgb values
+	"""
+
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
