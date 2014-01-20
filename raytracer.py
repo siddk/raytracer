b0279e4feae20280498f902b98a356b2b08f8193
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-01-20 02:02:27 -0800

    Adding draw function

--- raytracer.py
+++ raytracer.py
@@ -30,6 +30,77 @@ def draw(outputFile, scene):
 		pixelBuffer += pack('BBB', pixel.b, pixel.g, pixel.r)
 	output.write(pixelBuffer)
 
+def getPixel(x, y, scene):
+	"""
+	Performs the raytracing logic on a per pixel basis. Returns color of each pixel
+	"""
+
+	#Instantiate variables for red, green, blue values that make up pixel,
+	#set fields for constant coefficient
+	red = 0
+	green = 0
+	blue = 0
+	coef = 1.0
+	level = 0
+	
+	#Create Ray coming from behind point, with direction vector set to 1
+	view = Ray(Point(x, y, -1000),
+			   Vect(0.0, 0.0, 1.0))
+	
+
+	while True:
+		if coef > 0.0 and level < 10: 
+			t = 2000.0
+			currentSphere = None
+			spheres = scene.getSpheres()
+			# determine if the ray hits any of the spheres in the scene
+			for sphere in spheres:
+				isIntersected, t = sphereIntersect(view, sphere, t)
+				if isIntersected:
+					currentSphere = sphere
+			if currentSphere == None:
+				break
+			newStart = view.getStart() + view.getDir() * t
+			n = normal(newStart, currentSphere.getPosition())
+			temp = n * n
+			if temp == 0.0:
+				break
+			temp = 1.0 / math.sqrt(temp)
+			n = n * temp
+			currentMaterial = scene.getMaterialFromId(currentSphere.getMaterial())
+			if currentMaterial == None:
+				break
+			lights = scene.getLights()
+			for light in lights:
+				dist = normal(light.getPosition(), newStart)
+				if n * dist <= 0.0:
+					continue
+				a = math.sqrt(dist * dist)
+				if a <= 0.0:
+					continue
+				lightRay = Ray(newStart, dist * (1/a))
+				isShadowed = False
+				for sphere in spheres:
+					isIntersected, a = sphereIntersect(lightRay, sphere, a)
+					if isIntersected:
+						isShadowed = True
+						break
+				if not isShadowed:
+					lambert = (lightRay.getDir() * n) * coef
+					red += lambert * currentMaterial.getDiffuse().r * light.getIntensity().r
+					green += lambert * currentMaterial.getDiffuse().g * light.getIntensity().g
+					blue += lambert * currentMaterial.getDiffuse().b * light.getIntensity().b
+			coef = coef * currentMaterial.getReflection()
+			refl = 2.0 * (view.getDir() * n)
+			view.start = newStart
+			view.dir = view.dir - n * refl
+
+			level += 1
+		else:
+			break
+	return Color(min(red * 255, 255), min(green * 255, 255), min(blue * 255, 255)) 
+
+
 if __name__ == "__main__":
 	"""
 	Reads in file from std in, parses it for scene, then draws the new image.
